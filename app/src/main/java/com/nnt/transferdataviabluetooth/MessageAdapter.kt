564e58ee656720644806555a0accc54c59945fd6
thangnguyen <thangnguyen@okxe.vn>
2022-05-09 17:21:40 +0700

    fix bug: my message layout alight start instead end

--- app/src/main/java/com/nnt/transferdataviabluetooth/MessageAdapter.kt
+++ app/src/main/java/com/nnt/transferdataviabluetooth/MessageAdapter.kt
@@ -4,19 +4,37 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.nnt.transferdataviabluetooth.databinding.ItemMessageBinding
+import com.nnt.transferdataviabluetooth.databinding.ItemMyMessageBinding
+import com.nnt.transferdataviabluetooth.databinding.ItemOtherMessageBinding
 
-class MessageAdapter(private val messages: ArrayList<Message>): RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
-    class ViewHolder(val binding: ItemMessageBinding): RecyclerView.ViewHolder(binding.root)
+class MessageAdapter(private val messages: ArrayList<Message>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class MyMessageViewHolder(val binding: ItemMyMessageBinding): RecyclerView.ViewHolder(binding.root)
+    class OtherMessageViewHolder(val binding: ItemOtherMessageBinding): RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = parent.context.getSystemService(LayoutInflater::class.java)
-        return ViewHolder((ItemMessageBinding.inflate(inflater)))
+        return if(viewType == MY_MESSAGE_TYPE){
+            MyMessageViewHolder((ItemMyMessageBinding.inflate(inflater)))
+
+        } else {
+            OtherMessageViewHolder((ItemOtherMessageBinding.inflate(inflater)))
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val message = messages[position]
-        holder.binding.message = message
+        if (holder is MyMessageViewHolder){
+            holder.binding.message = message
+        }
+        else if(holder is OtherMessageViewHolder){
+            holder.binding.message = message
+
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (messages[position].isMyMessage) MY_MESSAGE_TYPE
+        else OTHER_MESSAGE_TYPE
     }
 
     override fun getItemCount(): Int {
@@ -32,5 +50,10 @@ class MessageAdapter(private val messages: ArrayList<Message>): RecyclerView.Ada
     fun clearData(){
         this.messages.clear()
         notifyDataSetChanged()
+    }
+
+    companion object {
+        private const val MY_MESSAGE_TYPE = 1
+        private const val OTHER_MESSAGE_TYPE = 2
     }
 }
